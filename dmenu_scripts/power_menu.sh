b386ac3378e9eb3e6f5ef4588d7ddb6344d8882b
Arun.R.R <arun90rr@gmail.com>
2021-10-07 22:02:11 +0530

    Add power menu script

--- dmenu_scripts/power_menu.sh
+++ dmenu_scripts/power_menu.sh
@@ -7,20 +7,20 @@ term=alacritty
 # Add commands for the power menu
 declare -A commands
 
-commands+=(['Shutdown']='sudo systemctl poweroff'
-	   ['Restart']='sudo systemctl reboot' 
-	   ['Sleep']='sudo systemctl suspend' 
+commands+=(['Shutdown']="sudo systemctl poweroff"
+	   ['Restart']="sudo systemctl reboot"
+	   ['Sleep']="sudo systemctl suspend"
 	)
 
 # Select a command 
 selected_command=$(for key in "${!commands[@]}"
 		   do
 			   echo $key
-		   done | dmenu -i -l 5 -p "Select a command"
+		   done | rofi -dmenu -i -p "Select a command"
 		 )
 
 # Execute the selected command
 if [ -v commands[$selected_command] ];then
-	$term -e "${commands[$selected_command]}"
+   $(${commands[$selected_command]})
 fi
 
